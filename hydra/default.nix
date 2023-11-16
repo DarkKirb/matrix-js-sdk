47889fcb9e4d47f0da68d7d9b277adefbbda4880
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-11-16 10:32:10 +0100

    fix ci’s default.nix

--- hydra/default.nix
+++ hydra/default.nix
@@ -1,16 +1,1 @@
-{
-  matrix-js-sdk,
-  nixpkgs,
-  system,
-}: let
-  pkgs = import nixpkgs {
-    inherit system;
-    config = {
-      allowUnfree = true;
-      allowUnsupportedSystem = true;
-    };
-  };
-  ci = import matrix-js-sdk {inherit pkgs;};
-  isReserved = n: n == "lib" || n == "overlays" || n == "modules";
-in
-  pkgs.lib.filterAttrs (name: _: !(isReserved name)) ci
+{matrix-js-sdk}: (import matrix-js-sdk).hydraJobs
