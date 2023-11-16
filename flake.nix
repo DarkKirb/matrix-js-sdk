8e80dbee6ca691f98d9fc50e24404140049b1b9d
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-11-16 09:41:50 +0100

    switch devshell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux"];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devshells.default.devshell.packages = with pkgs; [
+            nodejs yarn yarn2nix
+        ];
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
--- shell.nix
+++
@@ -1,6 +0,0 @@
-let pkgs = import <nixpkgs> {};
-in with pkgs; pkgs.mkShell {
-    nativeBuildInputs = [
-        nodejs yarn
-    ];
-}
