38f4a3e977233d8fc0c74469205c6d4914c6a555
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-11-16 10:26:59 +0100

    hook up hydra for ci

--- flake.nix
+++ flake.nix
@@ -47,9 +47,9 @@
         formatter = pkgs.alejandra;
       };
       flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
+        hydraJobs = {
+          inherit (inputs.self) devShells packages formatter;
+        };
       };
     };
 }
